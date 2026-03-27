5d022319c66147831c38a209e590be73407ee827
Leandro <leandrogomesdonascimento@gmail.com>
2026-03-27 01:40:12 -0300

    Otimização de código.

--- estrutura_de_dados_e_complexidade_de_algoritmos/alogritmos_de_ordenacao.py
+++ estrutura_de_dados_e_complexidade_de_algoritmos/alogritmos_de_ordenacao.py
@@ -28,39 +28,37 @@ def selection_sort(arr):
             arr[indice_menor] = aux
 
 def ler_numeros_por_linha(caminho):
-        return np.loadtxt(caminho, dtype=int)
+    return np.loadtxt(caminho, dtype=int)
+
+def ordenar_e_cronometrar(arr, sort_function: callable):
+    inicio_etapa = time.time()
+    sort_function(arr)
+    fim_etapa = time.time()
+    mostrar_msg_tempo("Tempo de ordenação " + sort_function.__name__, fim_etapa - inicio_etapa)
+    # print("Array após a ordenação:", arr)
+    return [sort_function.__name__, len(arr), fim_etapa - inicio_etapa]
 
 # Marca o início de toda a tarefa a ser cronometrada.
 inicio_tarefa = time.time()
 resultados = []
 
 # Leitura dos arquivos de entrada.
-
 diretorio = './instancias-num/' # Diretório atual
 entradas_in = [f for f in os.listdir(diretorio) if f.endswith('.in')]
 for entrada in entradas_in:
+    inicio_leitura = time.time()
     entrada_array = ler_numeros_por_linha(f'{diretorio}{entrada}')
     #print("Array antes da ordenação:", entrada_array)
-    fim_etapa = time.time()
-    mostrar_msg_tempo("Tempo de leitura de dados", fim_etapa - inicio_tarefa)
+    mostrar_msg_tempo("Tempo de leitura de dados", time.time() - inicio_leitura)
 
     # inicio da ordenação usando o Selection Sort
-    inicio_etapa = time.time()
-    selection_array = entrada_array.copy()
-    selection_sort(selection_array)
-    fim_etapa = time.time()
-    mostrar_msg_tempo("Tempo de ordenação Selection Sort", fim_etapa - inicio_etapa)
-    resultados.append(["selection", len(entrada_array), fim_etapa - inicio_etapa])
-    #print("Array após a ordenação:", selection_array)
+    resultados.append(ordenar_e_cronometrar(entrada_array.copy(), selection_sort()))
 
     # inicio da ordenação usando o Insertion Sort
-    inicio_etapa = time.time()
-    insertion_array = entrada_array.copy()
-    insertion_sort(insertion_array)
-    fim_etapa = time.time()
-    mostrar_msg_tempo("Tempo de ordenação Insertion Sort", fim_etapa - inicio_etapa)
-    resultados.append(["insertion", len(entrada_array), fim_etapa - inicio_etapa])
-    #print("Array após a ordenação:", insertion_array)
+    resultados.append(ordenar_e_cronometrar(entrada_array.copy(), insertion_sort()))
+
+    # inicio da ordenação usando Quick Sort
+    # resultados.append(ordenar_e_cronometrar(entrada_array.copy(), quick_sort()))
 
 print("Resultados após a ordenação:", resultados)
 # Marca o fim da tarefa e mostra o tempo.
